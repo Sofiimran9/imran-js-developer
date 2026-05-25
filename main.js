24070c5f506ee67882c4ecbf0f62202041b30d9d
AronWebSolutionPVTLTD <44797871+AronWebSolutionPVTLTD@users.noreply.github.com>
2026-05-25 13:20:47 +0530

    new practice

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ let products = [
     {name:"Jacket", price:"2000", Size:"M", Color:"Black"},
     {name:"Shoes", price:"1500", Size:"L", Color:"Black"}
 ];
-cc
 let result = products.filter(product => product.filter.Size==="M");
 console.log(result); 
