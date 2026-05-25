9d34488bfc045a7b7b818cc9e10bfb6d29de007b
AronWebSolutionPVTLTD <44797871+AronWebSolutionPVTLTD@users.noreply.github.com>
2026-05-25 13:16:51 +0530

    code updated

---
+++ main.js
@@ -0,0 +1,8 @@
+let products = [
+    {name:"Tshirt", price:"500", Size:"S", Color:"Black"},
+    {name:"Jacket", price:"2000", Size:"M", Color:"Black"},
+    {name:"Shoes", price:"1500", Size:"L", Color:"Black"}
+];
+
+let result = products.filter(product => product.filter.Size==="M");
+console.log(result);
